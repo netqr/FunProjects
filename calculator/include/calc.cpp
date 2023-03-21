2af533d9962bacd31c69c21aadadff30657dc91f
netqr <yesmaileasy@gmail.com>
2023-03-21 22:07:25 +0000

    Added calculator functions into calc namespace

--- calculator/include/calc.cpp
+++ calculator/include/calc.cpp
@@ -1,17 +1,19 @@
 #include "calc.h"
 
-float add(float num1, float num2) {
-	return num1 + num2;
-}
+namespace calc {
+	float add(float num1, float num2) {
+		return num1 + num2;
+	}
 
-float sub(float num1, float num2) {
-	return num1 - num2;
-}
+	float sub(float num1, float num2) {
+		return num1 - num2;
+	}
 
-float mul(float num1, float num2) {
-	return num1 * num2;
-}
+	float mul(float num1, float num2) {
+		return num1 * num2;
+	}
 
-float div(float num1, float num2) {
-	return num1 / num2;
+	float div(float num1, float num2) {
+		return num1 / num2;
+	}
 }
--- calculator/include/calc.h
+++ calculator/include/calc.h
@@ -1,10 +1,12 @@
 #pragma once
 
 
-float add(float num1, float num2);
+namespace calc {
+	float add(float num1, float num2);
 
-float sub(float num1, float num2);
+	float sub(float num1, float num2);
 
-float mul(float num1, float num2);
+	float mul(float num1, float num2);
 
-float div(float num1, float num2);
+	float div(float num1, float num2);
+}
