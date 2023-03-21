9218282e5b9b58784a5f6f2191f2c28bcf09cf1c
netqr <yesmaileasy@gmail.com>
2023-03-21 22:32:20 +0000

    Added code to main.cpp to interact with the calc libary, allows for input

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
+#include <string>
+
 #include "include/calc.h"
 
+
 int main() {
-	std::cout << "Hello, World!\n";
+	float num1;
+	float num2;
+	char operation;
+	float result;
+
+	std::cout << "Input first number: ";
+	std::cin >> num1;
+	std::cout << "\n";
+	std::cout << "Input second number: ";
+	std::cin >> num2;
+	std::cout << "\n";
+	std::cout << "Input desired operation: ";
+	std::cin >> operation;
+	std::cout << "\n";
+
+	switch (operation) {
+		case '+':
+			result = calc::add(num1, num2);
+			std::cout << num1 << " + " << num2 << " = " << result;
+			break;
+		case '-':
+			result = calc::sub(num1, num2);
+			std::cout << num1 << " - " << num2 << " = " << result;
+			break;
+		case '*':
+			result = calc::mul(num1, num2);
+			std::cout << num1 << " * " << num2 << " = " << result;
+			break;
+		case '/':
+			result = calc::div(num1, num2);
+			std::cout << num1 << " / " << num2 << " = " << result;
+			break;
+		default:
+			std::cout << "Invalid operand!\n";
+			break;
+	}
 
 	return 0;
 }
