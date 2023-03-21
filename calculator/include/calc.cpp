d1ea70bb72acaaf1947b00606075dc69dd798f3e
netqr <yesmaileasy@gmail.com>
2023-03-21 21:50:26 +0000

    Fixed include error without quotation and added ; to end of returns in functions

--- calculator/include/calc.cpp
+++ calculator/include/calc.cpp
@@ -1,17 +1,17 @@
-#include calc.h
+#include "calc.h"
 
 float add(float num1, float num2) {
-	return num1 + num2
+	return num1 + num2;
 }
 
 float sub(float num1, float num2) {
-	return num1 - num2
+	return num1 - num2;
 }
 
 float mul(float num1, float num2) {
-	return num1 * num2
+	return num1 * num2;
 }
 
 float div(float num1, float num2) {
-	return num1 / num2
+	return num1 / num2;
 }
