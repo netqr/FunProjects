daf5e819040ce058ba9f7ca4e071212eaa5a1ef2
netqr <yesmaileasy@gmail.com>
2023-03-30 18:24:40 +0100

    Cleanup by removing build directory in git repository

--- projectileMotion/build/include/CMakeFiles/calc.dir/calc.cpp.o.d
+++
@@ -1,4 +0,0 @@
-include/CMakeFiles/calc.dir/calc.cpp.o: \
- /home/jh/dev/FunProjects/calculator/include/calc.cpp \
- /usr/include/stdc-predef.h \
- /home/jh/dev/FunProjects/calculator/include/calc.h
